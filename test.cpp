26fe97b6ed228dbfadbef7e942a7b641b7ccfd37
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-07-21 10:52:11 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,9 +1,13 @@
+//Arduino IDE
+//tools > Board and select your Arduino board model.
+//Tools > Port and select the port your Arduino is connected to.
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1000);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);                      
 }
